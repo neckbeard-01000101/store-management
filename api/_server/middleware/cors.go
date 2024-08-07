f1fdfd80e3ec8007db10e8c14f6e584d4734623f
neckbeard-01000101 <mohammedredha908@gmail.com>
2024-08-07 13:40:59 +0300

    files fix

--- api/_server/middleware/cors.go
+++
@@ -1,9 +0,0 @@
-package middleware
-
-import "net/http"
-
-func EnableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
-}
--- api/_server/models/storagetype.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Storage struct {
-	TypeOfProduct string `json:"type_of_product"`
-	Quantity      int    `json:"quantity"`
-	Color         string `json:"product_color"`
-	Size          string `json:"product_size"`
-}
